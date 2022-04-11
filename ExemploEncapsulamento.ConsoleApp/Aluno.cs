9ba9e2235ea9391166f9ec1826c4cc4032d598aa
Henckemaier <eduhenckemaier@outlook.com>
2022-04-11 08:59:52 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploEncapsulamento.ConsoleApp/Aluno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEncapsulamento.ConsoleApp
+{
+    class Aluno
+    {
+        //atributos
+        private double nota1, nota2;
+
+        //media
+        private double media() //deixamos private pois apenas o Aluno.cs vai utilizar
+        {
+            return (nota1 + nota2)/2;
+        }
+
+        //Mensagem
+        public void mensagem()
+        {
+            Console.WriteLine("Informe a primeira nota");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a segunda nota");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A média é " + media());
+        }
+    }
+}
---
+++ ExemploEncapsulamento.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExemploEncapsulamento.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Encapsulamento é restringir o acesso a informação que vc tem suas classes
+        {
+            Aluno a = new Aluno();
+            a.mensagem();
+
+            Console.ReadLine();
+        }
+    }
+}
